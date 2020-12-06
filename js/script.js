366f016b61ed21bb0b615fc26b452e4e1790b383
Sapik <sapik_19@mail.ru>
2020-12-06 22:11:18 +0300

    Переделал множество элементов

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		// сколько слайдеров листает
 		slidesToScroll: 1,
 		// автоматическая прокрутка
-		autoplay: true,
+		autoplay: false,
 		// скорость прокрутки
 		autoplaySpeed: 3500,
 		// включаем стрелки
